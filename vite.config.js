79831bb4fa6a65d55e9fc64fbdb0e048819eb42e
lapLGH <luis-enrique06@outlook.com>
2025-02-08 12:30:32 -0600

    update/gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://luis-gn006.github.io/web_project_around_auth/',
   plugins: [react()],
   server: {
     port: 3000,
